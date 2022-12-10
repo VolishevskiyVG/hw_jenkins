458b30860257f52510e3699a58deb58256338ac1
Vasiliy Volishevskiy <Volishevski.V@gmail.com>
2022-12-10 13:44:24 +0300

    added qa test

--- src/test/java/tests/PageObjectsAutomationPracticeForm.java
+++ src/test/java/tests/PageObjectsAutomationPracticeForm.java
@@ -1,42 +1,46 @@
 package tests;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import static tests.TestData.*;
 
+import static io.qameta.allure.Allure.step;
+import static tests.TestData.*;
 
 
 public class PageObjectsAutomationPracticeForm extends TestBase {
 
     @Test
+    @DisplayName("Заполнение формы регистрации")
     void fillFormTest() {
-
-
-        registrationPage.openPage()
-                .setFirstName(userName)
-                .setLastName(lastName)
-                .setEmail(email)
-                .setGender(gender)
-                .setPhone(phone)
-                .setBirthDate(day, month, year)
-                .setSubjects(subject)
-                .setLoadPicture(file)
-                .setHobbiesWrapper(hobbies)
-                .setAddress(address)
-                .setState(state)
-                .setCity(city)
-                .pressSubmit();
-
-        registrationPage.verifyResultsModalAppears()
-                .verifyResult("Student Name", userName+" "+lastName)
-                .verifyResult("Student Email", email)
-                .verifyResult("Gender", gender)
-                .verifyResult("Mobile", phone)
-                .verifyResult("Date of Birth", day + " "+month+","+year)
-                .verifyResult("Subjects", subject)
-                .verifyResult("Hobbies", hobbies)
-                .verifyResult("Picture", "image_2022-11-11_17-13-46.png")
-                .verifyResult("Address", address)
-                .verifyResult("State and City", state+" "+city);
+        step("Заполнение формы регистрации", () -> {
+            registrationPage.openPage()
+                    .setFirstName(userName)
+                    .setLastName(lastName)
+                    .setEmail(email)
+                    .setGender(gender)
+                    .setPhone(phone)
+                    .setBirthDate(day, month, year)
+                    .setSubjects(subject)
+                    .setLoadPicture(file)
+                    .setHobbiesWrapper(hobbies)
+                    .setAddress(address)
+                    .setState(state)
+                    .setCity(city)
+                    .pressSubmit();
+        });
+        step("Верификация формы регистрации", () -> {
+            registrationPage.verifyResultsModalAppears()
+                    .verifyResult("Student Name", userName + " " + lastName)
+                    .verifyResult("Student Email", email)
+                    .verifyResult("Gender", gender)
+                    .verifyResult("Mobile", phone)
+                    .verifyResult("Date of Birth", day + " " + month + "," + year)
+                    .verifyResult("Subjects", subject)
+                    .verifyResult("Hobbies", hobbies)
+                    .verifyResult("Picture", "image_2022-11-11_17-13-46.png")
+                    .verifyResult("Address", address)
+                    .verifyResult("State and City", state + " " + city);
+        });
 
 
     }
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,7 +1,11 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import pages.RegistrationPage;
 
 public class TestBase {
@@ -11,5 +15,17 @@ public class TestBase {
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
+    }
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+    }
+    @AfterEach
+    void addAttachments(){
+        // screenshot
+        // page source
+        // console log
+        // video
     }
 }
